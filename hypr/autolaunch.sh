81a21d8c627ec4424ef9fd7a87b2c0fa520ff19f
ImStyrene <ImStyrene@protonmail.com>
2025-08-29 17:28:45 -0400

    Add files via upload

---
+++ hypr/autolaunch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# SWWW Wallpaper Manager
+swww-daemon &
+swww img ~/Pictures/Wallpapers/vecteezy_winter-wallpaper-for-desktop-with-flat-design-style-winter_11491443.png &
+
+# Waybar
+waybar &
+
+# SwayNC
+swaync &
+
+# Hyprlock
+hyprlock &
+
+# Hypridle
+hypridle &
